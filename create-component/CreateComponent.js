ef7d6047bd84aa3dcd3f2a1d0d20042965e2e393
Unknown <pethaudi@yahoo.de>
2018-11-14 12:41:44 +0100

    last commit on windows

--- create-component/CreateComponent.js
+++ create-component/CreateComponent.js
@@ -1,2 +1,16 @@
 #!/usr/bin/env node
 
+const fs = require("./../services/FileManager.js");
+const data = require("./Strings.js");
+
+let homefolder = "./";
+let projectname = "";
+let [,,...args] = process.argv;
+
+console.log(__dirname)
+console.log(fs.fromDir("index.js"));
+
+function main(args) {
+
+}
+
--- services/FileManager.js
+++ services/FileManager.js
@@ -3,18 +3,18 @@ const path = require("path");
 
 class FileManager {
     static createFile(name, content = "") {
-        fs.appendFile(name, content, function(err) {
-            if(err){
+        fs.appendFile(name, content, function (err) {
+            if (err) {
                 throw err;
             }
 
             console.log("created " + name);
-            
+
         });
     }
 
     static createDirectory(name) {
-        if(!fs.existsSync(name)){
+        if (!fs.existsSync(name)) {
             fs.mkdirSync(name);
         }
     }
@@ -22,6 +22,48 @@ class FileManager {
     static getParentFolderName(currentfolder) {
         return path.dirname(filename).split(path.sep).pop();
     }
+
+    static findFile(filename, counter = 5) {
+        console.log(filename + " " + counter);
+        if (counter < 0) {
+            console.log("counter is to low");
+            return "";
+        }
+
+        if (!fs.exists("./" + filename)) {
+            console.log(!fs.exists("./" + filename));
+            console.log("file does not exist");
+            counter--;
+            return FileManager.findFile("../" + filename, counter);
+        }
+        else {
+            return filename;
+        }
+    }
+
+static fromDir(filter, startPath = "./") {
+
+    //console.log('Starting from dir '+startPath+'/');
+
+    if (!fs.existsSync(startPath)) {
+        console.log("no dir ", startPath);
+        return;
+    }
+
+    var files = fs.readdirSync(startPath);
+    console.log(files);
+    for (var i = 0; i < files.length; i++) {
+        var filename = path.join(startPath, files[i]);
+        console.log(filename);
+        var stat = fs.lstatSync(filename);
+        if (stat.isDirectory()) {
+            FileManager.fromDir(filename, filter); //recurse
+        }
+        else if (filename.indexOf(filter) >= 0) {
+            console.log('-- found: ', filename);
+        };
+    };
+}
 }
 
 module.exports = FileManager;
